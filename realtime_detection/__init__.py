17bb501390ef71740237141aa6619518e2482d12
eriieii <eriiferdian92@gmail.com>
2022-11-20 23:29:59 +0700

    oop

--- realtime_detection/__init__.py
+++ realtime_detection/__init__.py
@@ -10,62 +10,77 @@ Scale : Dirasakan (Skala MMI): II Enggano
 from bs4 import BeautifulSoup
 import requests
 
-def extract_data():
 
-    try:
-        content = requests.get('https://www.bmkg.go.id/')
-        content.status_code
+class EarthQuakeIndo:
+    def __init__(self):
+        self.description = "Latest Earthquake Data Based on BMKG"
+        self.result = None
+    def extract_data(self):
 
-    except Exception:
-        return None
-    if content.status_code == 200:
-        soup = BeautifulSoup(content.text, 'html.parser')
-        result = soup.find('span', {'class': 'waktu'})
-        result = result.text.split(', ')
-        date = result[0]
-        time = result[1]
+        try:
+            content = requests.get('https://www.bmkg.go.id/')
+            content.status_code
 
+        except Exception:
+            return None
 
-        result = soup.find('div', {'class': 'col-md-6 col-xs-6 gempabumi-detail no-padding'})
-        result = result.findChildren('li')
+        if content.status_code == 200:
+            soup = BeautifulSoup(content.text, 'html.parser')
 
-        i = 0
-        for res in result:
-            #print(i, res)
-            if i == 1:
-                magnitudo = res.text
-            elif i == 2:
-                depth = res.text
-            elif i == 3:
-                geo = res.text.split(' - ')
-                ls = geo[0]
-                bt = geo[1]
-            elif i == 4:
-                location = res.text
-            elif i == 5:
-                scale = res.text
-            i = i + 1
+            result = soup.find('span', {'class': 'waktu'})
+            result = result.text.split(', ')
+            date = result[0]
+            time = result[1]
 
 
-    result = dict()
-    result['date'] = date #"16 November 2022"
-    result['time'] = time #"16:22:45 WIB"
-    result['magnitudo'] = magnitudo #"5.3"
-    result['depth'] = depth #"26 km"
-    result['geo'] = {
-        'ls' : ls,
-        'bt' : bt,
-        'location' : location,
-    }
-    result['scale'] = scale
+            result = soup.find('div', {'class': 'col-md-6 col-xs-6 gempabumi-detail no-padding'})
+            result = result.findChildren('li')
 
-    return result
+            i = 0
+            for res in result:
+                #print(i, res)
+                if i == 1:
+                    magnitudo = res.text
+                elif i == 2:
+                    depth = res.text
+                elif i == 3:
+                    geo = res.text.split(' - ')
+                    ls = geo[0]
+                    bt = geo[1]
+                elif i == 4:
+                    location = res.text
+                elif i == 5:
+                    scale = res.text
+                i = i + 1
 
 
-def view_data(result):
-    print("Data Gempa Terakhir Berdasarkan BMKG")
-    print(f"Tanggal {result['date']} Pada Jam {result['time']}")
-    print(f"Dengan Kekuatan Gempa Sebesar {result['magnitudo']} magnitudo")
-    print(f"Di Kedalaman {result['depth']}")
-    print(f"Lokasi di {result['geo']['ls']} - {result['geo']['bt']}")
-    print(f"{result['geo']['location']} {result['scale']}")
+            result = dict()
+            result['date'] = date #"16 November 2022"
+            result['time'] = time #"16:22:45 WIB"
+            result['magnitudo'] = magnitudo #"5.3"
+            result['depth'] = depth #"26 km"
+            result['geo'] = {
+                'ls' : ls,
+                'bt' : bt,
+                'location' : location,
+            }
+            result['scale'] = scale
+
+            self.result = result
+        else:
+            return None
+
+
+    def view_data(self):
+        #print("Data Gempa Terakhir Berdasarkan BMKG")
+        print(f"Tanggal {self.result['date']} Pada Jam {self.result['time']}")
+        print(f"Dengan Kekuatan Gempa Sebesar {self.result['magnitudo']} magnitudo")
+        print(f"Di Kedalaman {self.result['depth']}")
+        print(f"Lokasi di {self.result['geo']['ls']} - {self.result['geo']['bt']}")
+        print(f"{self.result['geo']['location']} {self.result['scale']}")
+
+if __name__ == "__main__":
+    var = EarthQuakeIndo()
+    print(var.description)
+    var.extract_data()
+    var.view_data()
